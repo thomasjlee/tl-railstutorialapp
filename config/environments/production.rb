633d6f0b518357d19ec25d6fb0409173990060ac
Thomas Lee <leethomasjae@gmail.com>
2018-11-17 15:53:44 -0600

    Update ActionMailer host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://morning-beach-61907.herokuapp.com/'
+  host = 'https://tlee-rails-tutorial-sample-app.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
